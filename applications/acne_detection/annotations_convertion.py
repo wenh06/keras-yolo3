fffb38e62ae5b80d4afd529ccccd9f2049e9e938
WEN Hao <wenh06@gmail.com>
2019-12-20 21:23:25 +0800

    add voc annotations to yolo annotations conversion method, not finished

--- applications/acne_detection/annotations_convertion.py
+++ applications/acne_detection/annotations_convertion.py
@@ -17,11 +17,11 @@ _ALL_LESION_TYPES = ['fore', 'cold_sore',]
 _VALID_LEISION_TYPES = ['fore',]
 
 
-def xml_to_csv(xml_path:str, save_csv_path:Optional[str]=None) -> pd.DataFrame:
+def xml_to_csv(xml_dir:str, save_csv_path:Optional[str]=None) -> pd.DataFrame:
     """
     """
     xml_list = []
-    for xml_file in glob.glob(xml_path + '/*.xml'):
+    for xml_file in glob.glob(os.path.join(xml_dir, '*.xml')):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         if len(root.findall('object')) == 0:
@@ -38,8 +38,8 @@ def xml_to_csv(xml_path:str, save_csv_path:Optional[str]=None) -> pd.DataFrame:
                 'difficult': member.find('difficult').text if member.find('difficult') is not None else '',
                 'xmin': int(member.find('bndbox').find('xmin').text),
                 'ymin': int(member.find('bndbox').find('ymin').text),
-                'xmax': int(member.find('bndbox').find('xmax').text)-1,
-                'ymax': int(member.find('bndbox').find('ymax').text)-1,
+                'xmax': int(member.find('bndbox').find('xmax').text),
+                'ymax': int(member.find('bndbox').find('ymax').text),
             }
             xml_list.append(values)
     column_names = ['filename', 'width', 'height', 'segmented', 'class', 'pose', 'truncated', 'difficult', 'xmin', 'ymin', 'xmax', 'ymax']
@@ -53,7 +53,48 @@ def xml_to_csv(xml_path:str, save_csv_path:Optional[str]=None) -> pd.DataFrame:
     return xml_df
 
 
-def voc_to_yolo(voc_ann_path:str, yolo_save_path:str) -> NoReturn:
+def voc_to_yolo(voc_ann_path:str, yolo_save_dir:str) -> NoReturn:
     """
+    yolo annotation format:
+        <object-class> <x_center> <y_center> <width> <height>
+    ref.
+        [1] https://github.com/AlexeyAB/darknet#how-to-train-to-detect-your-custom-objects
+        [2] https://github.com/tzutalin/labelImg/blob/master/libs/yolo_io.py
     """
+    voc_filename = os.path.basename(voc_ann_path)
+    tree = ET.parse(voc_filename)
+    root = tree.getroot()
+
+    if len(root.findall('object')) == 0:
+        print('{} has no acne annotation'.format(voc_filename))
+        return
     
+    yolo_filename = voc_filename.replace('xml', 'txt')
+    with open(os.path.join(yolo_save_dir, yolo_filename), 'w') as yf:
+        img_width = int(root.find('size').find('width').text)
+        img_height = int(root.find('size').find('height').text)
+        for member in root.findall('object'):
+            class_idx = _ALL_LESION_TYPES.index(member.find('name').text)
+            difficult = member.find('difficult').text if member.find('difficult') is not None else ''
+            xmin = int(member.find('bndbox').find('xmin').text)
+            ymin = int(member.find('bndbox').find('ymin').text)
+            xmax = int(member.find('bndbox').find('xmax').text)
+            ymax = int(member.find('bndbox').find('ymax').text)
+            xcen = float((xmin + xmax)) / 2 / img_width
+            ycen = float((ymin + ymax)) / 2 / img_height
+            w = float((xmax - xmin)) / img_width
+            h = float((ymax - ymin)) / img_height
+            yf.write("%d %.6f %.6f %.6f %.6f\n" % (class_idx, xcen, ycen, w, h))
+
+
+def voc_to_yolo_in_batch(voc_dir:str, yolo_save_dir:str) -> NoReturn:
+    """
+    """
+    for xml_file in glob.glob(os.path.join(voc_dir, '*.xml')):
+        voc_to_yolo(xml_file, yolo_save_dir)
+
+
+def yolo_to_csv(yolo_dir:str, save_csv_path:Optional[str]=None) -> pd.DataFrame:
+    """
+    """
+    raise NotImplementedError
